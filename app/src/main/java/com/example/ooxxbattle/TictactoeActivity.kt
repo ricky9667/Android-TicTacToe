9197d7dc442953c73a57bb4caa1e99ca30da2308
Ricky Hu <ricky9667@gmail.com>
2019-12-06 19:56:29 +0800

    Leave Button implemented, fixed a few bugs

--- app/src/main/java/com/example/ooxxbattle/TictactoeActivity.kt
+++ app/src/main/java/com/example/ooxxbattle/TictactoeActivity.kt
@@ -16,13 +16,67 @@ class TictactoeActivity : AppCompatActivity(), View.OnClickListener {
     var lastClicked: View? = null
     var player1: String? = null
     var player2: String? = null
+    var grid: MutableList<String> = mutableListOf("","","","","","","","","")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tictactoe)
+    }
 
+    override fun onStart() {
+        super.onStart()
         getInfo()
-        initButtons()
+        startGame()
+    }
+
+    override fun onClick(v: View) {
+        val clickedId = v.id
+        when(clickedId) {
+            // when click leave btn
+            btn_leave.id ->
+                finish()
+
+            // when click restart btn (not fully implemented)
+            btn_restart.id -> {
+                Toast.makeText(this, "Restart: Not implemented", Toast.LENGTH_SHORT).show()
+                startGame()
+            }
+
+            // when click put here btn -> change player title/color
+            btn_confirm.id -> {
+
+                if(lastClicked == null)
+                    Toast.makeText(this, "Choose a block first!", Toast.LENGTH_SHORT).show()
+                else {
+                    lastClicked?.setBackgroundResource(android.R.drawable.btn_default)
+                    if (currentPlayer == 1) {
+                        (lastClicked as Button).text = "O"
+                        //                    markGrid(v,"O")   // with error
+                        currentPlayer = 2
+                        player2?.let { changeTitle(currentPlayer, it) }
+                    } else {
+                        (lastClicked as Button).text = "X"
+                        //                    markGrid(v,"X")   // with error
+                        currentPlayer = 1
+                        player1?.let { changeTitle(currentPlayer, it) }
+                    }
+                }
+            }
+
+            // when click grid btn
+            else -> {
+                lastClicked?.setBackgroundResource(android.R.drawable.btn_default)
+                if(currentPlayer == 1)
+                    v.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.p1_color))
+                else
+                    v.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.p2_color))
+                lastClicked = v
+            }
+        }
+    }
+
+    private fun startGame() {
+        initialize()
         chooseStarter()
 
     }
@@ -34,7 +88,8 @@ class TictactoeActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun initButtons() {
+    private fun initialize() {
+        // init buttons
         btn_confirm.setOnClickListener(this)
         btn_restart.setOnClickListener(this)
         btn_leave.setOnClickListener(this)
@@ -47,9 +102,22 @@ class TictactoeActivity : AppCompatActivity(), View.OnClickListener {
         btn_ru.setOnClickListener(this)
         btn_rm.setOnClickListener(this)
         btn_rd.setOnClickListener(this)
+
+        // init grid
+        grid = mutableListOf("","","","","","","","","")
+        btn_ld.text = ""
+        btn_lm.text = ""
+        btn_lu.text = ""
+        btn_md.text = ""
+        btn_mm.text = ""
+        btn_mu.text = ""
+        btn_rd.text = ""
+        btn_rm.text = ""
+        btn_ru.text = ""
     }
 
     private fun chooseStarter() {
+        currentPlayer = (1..2).random()
         if(currentPlayer == 1 && player1 != null) {
             changeTitle(currentPlayer, player1!!)
         }
@@ -74,33 +142,19 @@ class TictactoeActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    override fun onClick(v: View) {
-        val clickedId = v.id
-        when(clickedId) {
-            btn_restart.id -> Toast.makeText(this, "Restart: Not implemented", Toast.LENGTH_SHORT).show()
-            btn_leave.id -> Toast.makeText(this, "Leave: Not implemented", Toast.LENGTH_SHORT).show()
-            btn_confirm.id -> {
-
-                lastClicked?.setBackgroundResource(android.R.drawable.btn_default)
-                if(currentPlayer == 1) {
-                    (lastClicked as Button).text = "O"
-                    currentPlayer = 2
-                    player2?.let { changeTitle(currentPlayer, it) }
-                }
-                else {
-                    (lastClicked as Button).text = "X"
-                    currentPlayer = 1
-                    player1?.let { changeTitle(currentPlayer, it) }
-                }
-            }
-            else -> {
-                lastClicked?.setBackgroundResource(android.R.drawable.btn_default)
-                if(currentPlayer == 1)
-                    v.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.p1_color))
-                else
-                    v.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.p2_color))
-                lastClicked = v
-            }
+    private fun markGrid(v: View, player: String) {
+        val index = when (v) {
+            btn_lu -> 0
+            btn_mu -> 1
+            btn_ru -> 2
+            btn_lm -> 3
+            btn_mm -> 4
+            btn_rm -> 5
+            btn_ld -> 6
+            btn_md -> 7
+            btn_rd -> 8
+            else -> -1
         }
+        grid[index] = player
     }
 }
